1f54f7979591f545a526b7d81ec59cb7000cd421
Yhomira Yupayccana Lopa <yhomira.yupayccana@sofka.com.co>
2022-02-15 22:36:01 -0500

    usando api de imagenes

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,15 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
-
+  private apiKey:string='0jkSHTnKot936tFQa7GpnY0920msWghx'
   private _historial:string[]=[];
+  public resultados: any[]=[]
 
   get historial(){
 
     return[...this._historial]
+  }
+  constructor(private http:HttpClient){
+
   }
   buscarGifs(query:string=""){
     query=query.trim().toLocaleLowerCase();
@@ -17,7 +22,13 @@ export class GifsService {
       this._historial.unshift(query);
       this._historial=this._historial.splice(0,10);
     }
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=0jkSHTnKot936tFQa7GpnY0920msWghx&q=${query}&limit=10`)
+          .subscribe((resp:any)=>{
+            console.log(resp.data);
+            this.resultados=resp.data;
+          })
 
-    console.log(this._historial)
+
+
   }
 }
